efb1368b14b99a05d9fae8e868d9529c672f1ee1
josegab1515 <gabrielcity369@gmail.com>
2026-08-18 23:08:12 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetosBasicos/cadastroAluno/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetosBasicos.cadastroAluno
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
