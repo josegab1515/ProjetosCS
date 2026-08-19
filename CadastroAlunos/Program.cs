5693dbb8046d6970c5b46346e983303ad017ed80
josegab1515 <gabrielcity369@gmail.com>
2026-08-19 00:04:05 -0300

    Novos Projetos

---
+++ CadastroAlunos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroAlunos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!!!!");
+        }
+    }
+}
