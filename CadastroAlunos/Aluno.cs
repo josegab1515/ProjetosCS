91de7229bd5f22f322158d8b0d76671cfb0bd289
josegab1515 <gabrielcity369@gmail.com>
2026-08-19 22:21:07 -0300

    Cadastro de alunos feito

---
+++ CadastroAlunos/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroAlunos
+{
+    public class Aluno
+    {
+        public string? Nome { get; set; }
+        public int Idade { get; set; }
+        public string? Curso { get; set; }
+
+
+        public Aluno(string nome, int idade, string curso)
+        {
+            Nome = nome;
+            Idade = idade;
+            Curso = curso;
+        }
+    }
+}
--- CadastroAlunos/Program.cs
+++ CadastroAlunos/Program.cs
@@ -8,7 +8,19 @@ namespace CadastroAlunos
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello!!!!");
+            Aluno aluno1 = new Aluno(nome:"Jose Gabriel", idade:20, curso:"Big Data");
+
+            Console.WriteLine($"Nome: {aluno1.Nome}");
+            Console.WriteLine($"Idade: {aluno1.Idade}");
+            Console.WriteLine($"Curso: {aluno1.Curso}");
+           
+            Console.WriteLine("-------------------------------");
+
+            aluno1.Curso = "Sistemas Inteligentes";
+
+            Console.WriteLine($"Curso: {aluno1.Curso}");
+
+
         }
     }
 }
